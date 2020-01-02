669d6912a640def1be0d05536c0d25116dabe1f3
unknown <bcbeck@gmail.com>
2020-01-02 00:45:01 -0800

    Changes app text

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There, blah blah
-        
+          This is a fantastic test change to test our CI pipeline.
         </p>
         <a
           className="App-link"
